53049382e6cbce88a032fb50e4898dc97dd7175a
AlejandroSancho <34617@DESKTOP-QUD65H0>
2020-11-14 18:06:32 +0100

    Version Master final

--- src/main/java/com/mycompany/proyectofuturo/EntornosProyectoFururo.java
+++ src/main/java/com/mycompany/proyectofuturo/EntornosProyectoFururo.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.proyectofuturo;
 
-/**
- *
- * @author 34617
- */
+/*Ejercicio realizado por Alejandro Sancho
+Github -> Sancho-crab
+*/
 public class EntornosProyectoFururo {
         
        public static void main (String[]args){
